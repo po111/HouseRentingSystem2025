a7d5693b6998489a1d9b861cf42a642e9ece0aae
po111 <98609236+po111@users.noreply.github.com>
2025-10-27 15:53:51 +0200

    Layout tweaked

--- HouseRentingSystem2025/Controllers/HomeController.cs
+++ HouseRentingSystem2025/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace HouseRentingSystem2025.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
