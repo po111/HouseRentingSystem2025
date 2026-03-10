0d7d875bad776cd2cb02726ec4486b813ebfb66f
po111 <98609236+po111@users.noreply.github.com>
2026-03-10 12:10:24 +0200

    Repository added

---
+++ HouseRentingSystem2025.Infrastructure/Data/Common/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem2025.Infrastructure.Data.Common
+{
+    public interface IRepository
+    {
+        IQueryable<T> All<T>() where T : class;
+
+        IQueryable<T> AllReadOnly<T>() where T : class;
+    }
+}
---
+++ HouseRentingSystem2025.Infrastructure/Data/Common/Repository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem2025.Infrastructure.Data.Common
+{
+    public class Repository : IRepository
+    {
+
+        private readonly DbContext context;
+
+        public Repository(HouseRentingDbContext _context) 
+        {
+                context = _context;
+        }
+
+        private DbSet<T> DbSet<T>() where T : class
+        {
+            return context.Set<T>();
+        }
+        public IQueryable<T> All<T>() where T : class
+        {
+            return DbSet<T>();
+        }
+
+        public IQueryable<T> AllReadOnly<T>() where T : class
+        {
+            return DbSet<T>()
+                .AsNoTracking();
+                
+        }
+    }
+}
