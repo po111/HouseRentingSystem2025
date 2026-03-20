b9597f921ac8ba1a4c58cf1a8fb4a79532b83c5c
po111 <98609236+po111@users.noreply.github.com>
2026-03-20 10:13:03 +0200

    Agent Service added

---
+++ HouseRentingSystem.Core/Contracts/IAgentService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem2025.Core.Contracts
+{
+    public interface IAgentService
+    {
+        Task<bool> existById(string userId);
+    }
+}
---
+++ HouseRentingSystem.Core/Services/AgentService.cs
@@ -0,0 +1,20 @@
+﻿using HouseRentingSystem2025.Core.Contracts;
+using HouseRentingSystem2025.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HouseRentingSystem2025.Core.Services
+{
+    public class AgentService : IAgentService
+    {
+
+        private readonly HouseRentingDbContext 
+        public Task<bool> existById(string userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
